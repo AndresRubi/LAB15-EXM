cf991e2372afb3c196824c0e1444ca2f16940a60
Andres Rubi <andresrues@hotmail.com>
2015-02-27 00:15:28 -0600

    first commit

---
+++ Sho.cpp
@@ -0,0 +1,92 @@
+#include "Sho.h"
+
+Sho::Sho(SDL_Renderer* renderer,list<Personaje*> *personajes)
+{
+    mapa_texturas["left"] = new vector<SDL_Texture*>();
+    mapa_texturas["right"] = new vector<SDL_Texture*>();
+    mapa_texturas["walk_right"] = new vector<SDL_Texture*>();
+    mapa_texturas["walk_left"] = new vector<SDL_Texture*>();
+
+    mapa_texturas["right"]->push_back(IMG_LoadTexture(renderer,"Sho/standing/1.png"));
+    mapa_texturas["right"]->push_back(IMG_LoadTexture(renderer,"Sho/standing/2.png"));
+    mapa_texturas["right"]->push_back(IMG_LoadTexture(renderer,"Sho/standing/3.png"));
+    mapa_texturas["right"]->push_back(IMG_LoadTexture(renderer,"Sho/standing/4.png"));
+
+    mapa_texturas["left"]->push_back(IMG_LoadTexture(renderer,"Sho/standing_left/1.png"));
+    mapa_texturas["left"]->push_back(IMG_LoadTexture(renderer,"Sho/standing_left/2.png"));
+    mapa_texturas["left"]->push_back(IMG_LoadTexture(renderer,"Sho/standing_left/3.png"));
+    mapa_texturas["left"]->push_back(IMG_LoadTexture(renderer,"Sho/standing_left/4.png"));
+
+    mapa_texturas["walk_right"]->push_back(IMG_LoadTexture(renderer,"Sho/walk/1.png"));
+    mapa_texturas["walk_right"]->push_back(IMG_LoadTexture(renderer,"Sho/walk/2.png"));
+    mapa_texturas["walk_right"]->push_back(IMG_LoadTexture(renderer,"Sho/walk/3.png"));
+    mapa_texturas["walk_right"]->push_back(IMG_LoadTexture(renderer,"Sho/walk/4.png"));
+    mapa_texturas["walk_right"]->push_back(IMG_LoadTexture(renderer,"Sho/walk/5.png"));
+
+    mapa_texturas["walk_left"]->push_back(IMG_LoadTexture(renderer,"Sho/walk_left/1.png"));
+    mapa_texturas["walk_left"]->push_back(IMG_LoadTexture(renderer,"Sho/walk_left/2.png"));
+    mapa_texturas["walk_left"]->push_back(IMG_LoadTexture(renderer,"Sho/walk_left/3.png"));
+    mapa_texturas["walk_left"]->push_back(IMG_LoadTexture(renderer,"Sho/walk_left/4.png"));
+    mapa_texturas["walk_left"]->push_back(IMG_LoadTexture(renderer,"Sho/walk_left/5.png"));
+
+
+    vector_actual_str = "right";
+
+    rect.x = 0;
+    rect.y = 0;
+    id="player";
+    vida=5;
+    init(renderer,personajes);
+}
+
+void Sho::act()
+{
+    const Uint8* currentKeyStates = SDL_GetKeyboardState(NULL);
+
+    atacando = currentKeyStates[SDL_SCANCODE_SPACE];
+
+    if(currentKeyStates[SDL_SCANCODE_W])
+    {
+        rect.y--;
+    }
+    else if(currentKeyStates[SDL_SCANCODE_S])
+    {
+        rect.y++;
+    }
+
+    if(currentKeyStates[SDL_SCANCODE_D])
+    {
+        rect.x++;
+        setAnimacion("walk_right");
+    }
+    else if(currentKeyStates[SDL_SCANCODE_Z])
+    {
+        rect.x--;
+        setAnimacion("walk_left");
+    }
+    else
+    {
+        if(vector_actual_str == "walk_right")
+            vector_actual_str = "right";
+
+        if(vector_actual_str == "walk_left")
+            vector_actual_str = "left";
+    }
+
+    attackCheck();
+    if(attackCheck())
+    {
+        pentaKill+=1;
+        cout<<pentaKill<<endl;
+    }
+
+    if(pentaKill==3)
+        {
+            cout<<"Ganastes"<<pentaKill<<endl;
+        }
+}
+
+Sho::~Sho()
+{
+    //dtor
+}
